2a0ae4e333540c23a93ad80bc7894f131cff9d4a
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-03-25 21:57:40 +0600

    modified

--- src/component/BestSelling/BestSellingCard.jsx
+++ src/component/BestSelling/BestSellingCard.jsx
@@ -7,7 +7,7 @@ const BestSellingCard = ({sData,handleCart}) => {
         <div className='border w-full h-full border-green-400 rounded-xl p-4 space-y-1 shadow-xl bg-white transition-transform duration-300 hover:border-amber-400 hover:bg-red-100 hover:-translate-y-2 hover:shadow-amber-300/50'>
             <img className='rounded-xl' src={sData.strMealThumb} alt="" />
             <h2 className='font-bold'>Name: {sData.strMeal}</h2>
-            <p className='text-gray-600'>Food Region: {sData.strArea}</p>
+            <p className='text-gray-500'>Food Region: {sData.strArea}</p>
             <div className='flex justify-between'>
                 <h1 className='text-green-600 text-xl font-bold'>${generatedPrice}</h1>
                 <button onClick={()=> handleCart(sData)} className='btn btn-primary'>Add Cart</button>
